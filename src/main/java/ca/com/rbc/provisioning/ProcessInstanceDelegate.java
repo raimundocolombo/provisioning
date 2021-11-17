322ccf046119ac8d77cdeb07348e6d6b47435118
Raimundo Ferreira dos Santos Júnior <raimundo.junior@ntconsult.net>
2021-11-17 11:29:05 -0300

    DMN creation and other implementations.

---
+++ src/main/java/ca/com/rbc/provisioning/ProcessInstanceDelegate.java
@@ -0,0 +1,36 @@
+package ca.com.rbc.provisioning;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is an easy adapter implementation
+ * illustrating how a Java Delegate can be used
+ * from within a BPMN 2.0 Service Task.
+ */
+@Component("processInstance")
+public class ProcessInstanceDelegate implements JavaDelegate {
+ 
+  private final Logger LOGGER = Logger.getLogger(ProcessInstanceDelegate.class.getName());
+  
+  public void execute(DelegateExecution execution) throws Exception {
+    
+    LOGGER.info("\n\n  ... LoggerDelegate invoked by "
+            + "activityName='" + execution.getCurrentActivityName() + "'"
+            + ", activityId=" + execution.getCurrentActivityId()
+            + ", processDefinitionId=" + execution.getProcessDefinitionId()
+            + ", processInstanceId=" + execution.getProcessInstanceId()
+            + ", businessKey=" + execution.getProcessBusinessKey()
+            + ", executionId=" + execution.getId()
+            + ", variables=" + execution.getVariables()
+            + " \n\n");
+    
+    execution.setVariable("processInstanceId", execution.getProcessInstanceId());
+    
+    
+  }
+
+}
